3ef492050ac80546f82b1bdde69cf770c7d04efc
Arjun Krishna <arjun12345krishna@gmail.com>
2025-11-01 06:18:14 +0530

    fear : Refator Umbrella Fn

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,27 +5,19 @@ import { CandlesParams } from './types.js';
 import { formatToHumanReadableData } from './utils.js';
 
 
-(async () => {
-  try {
+async function getIndicators(duration: number, symbol: string) {
+  const params = {
+    start_time: Date.now() - (1000 * 60 * 60 * (duration === 5 ? 12 : 24 * 10)), // 12 hours for 5min, 10 days for 4hr
+    end_time: Date.now(),
+    symbol: symbol,
+    interval: duration,
+  } as CandlesParams;
 
-    // Test API connectivity
-    const pingResponse = await pingApi();
-    console.log('Ping response:', pingResponse);
+  const candlesData = await getCandleData(params);
+  console.table(formatToHumanReadableData(candlesData.data));
+  console.log('Total Candles fetched:', candlesData.data.length);
 
-
-    
-    const params = {
-      start_time: Date.now() - (1000 * 60 * 60 * 24 * 10), // 10 days ago
-      end_time: Date.now(),
-      symbol: 'SOL/INR',
-      interval: '240'
-    } as CandlesParams;
-
-    const candlesData = await getCandleData(params);
-    console.table(formatToHumanReadableData(candlesData.data));
-    console.log('Total Candles fetched:', candlesData.data.length);
-
-    const midPrices = calcMidPrice(candlesData.data.splice(-50)); // Calculate Mid Prices for last 50 candles
+  const midPrices = calcMidPrice(candlesData.data.splice(-50)); // Calculate Mid Prices for last 50 candles
     console.log('Mid Prices:', midPrices);
     console.log('Total Mid Prices fetched:', midPrices.length);
 
@@ -37,9 +29,26 @@ import { formatToHumanReadableData } from './utils.js';
     console.log('MACD:', macd);
     console.log('Total MACD values fetched:', macd.length);
 
+  return {
+    midPrices,
+    ema20,
+    macd
+  };
+}
+
+
+(async () => {
+  try {
+
+    // Test API connectivity
+    // const pingResponse = await pingApi();
+    // console.log('Ping response:', pingResponse);
+
+    await getIndicators(5, 'SOL/INR');
+    // await getIndicators(240, 'SOL/INR');
+
   } catch (error) {
-    console.error('Error fetching candles data:', error);
+    console.error('Error in main execution:', error);
   }
-  
   process.exit(0);
 })();
