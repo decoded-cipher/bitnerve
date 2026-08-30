b3b6708b52f674a22edfd419ba8e631cdfcaeba9
Arjun Krishna <arjun12345krishna@gmail.com>
2026-08-30 09:07:06 +0530

    refactor: remove Cloudflare worker configurations and related scripts from client package

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: '2024-04-03',
   devtools: { enabled: process.env.NODE_ENV !== 'production' },
   css: ['~/assets/css/main.css'],
   app: {
